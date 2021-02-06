b3dada63c2aff084918805a5781f6445c3374738
shfshanyue <xianger94@gmail.com>
2021-02-06 12:42:28 +0800

    core: detect lang support

--- md-read-text.ts
+++ md-read-text.ts
@@ -1,5 +1,16 @@
 import TurndownService from 'turndown'
+import { LANGUAGES } from './language'
+
 const { tables } = require('turndown-plugin-gfm')
+
+function detectLanguage (className: string): string {
+  for (const lang of LANGUAGES) {
+    if (new RegExp(`\\b${lang}\\b`).test(className)) {
+      return lang
+    }
+  }
+  return 'auto'
+}
 
 const turndownService = new TurndownService({
   emDelimiter: '*',
@@ -15,15 +26,16 @@ turndownService.addRule('autoLanguage', {
   filter (node, options) {
     return Boolean(
       options.codeBlockStyle === 'fenced' &&
-      node.nodeName === 'CODE' &&
-      (node.parentElement?.nodeName === 'PRE' || node.textContent?.includes('\n'))
+      node.nodeName === 'PRE' &&
+      node.firstChild &&
+      node.firstChild.nodeName === 'CODE'
     )
   },
 
   replacement (content, node, options) {
     node = node as HTMLElement
-    const className = node.getAttribute('class') || node.parentElement?.getAttribute('class') || ''
-    const language = (className.match(/language-(\S+)/) || [null, ''])[1]
+    const className = [node.className, node.firstElementChild?.className].join(' ')
+    const language = (className.match(/language-(\S+)/) || [null, detectLanguage(className)])[1]
     const code = node.textContent || ''
     const fence = options.fence
 
