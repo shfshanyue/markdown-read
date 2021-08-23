a6ba570ec34e8e049084f12e2263b05fc5369c36
shfshanyue <xianger94@gmail.com>
2021-08-23 17:29:04 +0800

    fix: zhihu link

--- platform/zhihu.ts
+++ platform/zhihu.ts
@@ -4,6 +4,14 @@ export const filter = (document: Document) => document.URL.startsWith('https://z
 
 export const processDocument = (document: Document) => {
   document.querySelectorAll('.Post-Sub, .ContentItem-time').forEach(el => el.remove())
+  document.querySelectorAll('.RichText a').forEach(a => {
+    const href = a.getAttribute('href')
+    if (href?.startsWith('https://link.zhihu')) {
+      const url = new URL(href)
+      const target = url.searchParams.get('target') || ''
+      a.setAttribute('href', target)
+    }
+  })
 }
 
 export const turndownPlugins = (turndown: TurndownService) => {}
